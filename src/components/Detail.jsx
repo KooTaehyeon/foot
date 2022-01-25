c850df8ac54593b004f5604845d213a6c47be6aa
Kth <vcro12123@naver.com>
2022-01-25 20:49:12 +0900

    디테일페이지 css작업중

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import './Detail.css';
+import imgC from './img/time@2x.png';
+import imgD from './img/serving@2x.png';
+import imgE from './img/kcal@2x.png';
+import imgF from './img/check_full@2x.png';
 const Detail = () => {
   const [ids, setIds] = useState([]);
   const { id } = useParams();
@@ -19,35 +23,60 @@ const Detail = () => {
   console.log(ids);
 
   return (
-    <div>
-      <img className='api_img' src={ids.picture} alt='사진' />
-      <div className='api_name'>{ids.name}</div>
-      <div>
-        <span className='api_inline'>{ids.cookingTime},</span>
-        <span className='api_inline'>{ids.servings},</span>
-        <span className='api_inline'>{ids.kcal}</span>
-      </div>
-      <div>
-        <span className='api_block'>재료</span>
-        <ul>
-          {ids.ingredients && ids.ingredients.map((index) => <li>{index}</li>)}
-        </ul>
-      </div>
-      <div>
-        <span className='api_block'>양념장</span>
-        <ul>{ids.spices && ids.spices.map((index) => <li>{index}</li>)}</ul>
-      </div>
-      <div>
-        <span className='api_block'>만들어봅시다!!</span>
-        <ul>
-          {ids.cookingSteps &&
-            ids.cookingSteps.map((index, i) => (
-              <li>
-                <div className='api_num'>{i}</div>
-                {index}
-              </li>
-            ))}
-        </ul>
+    <div className='details'>
+      <div className='details_info'>
+        <img className='api_img' src={ids.picture} alt='사진' />
+        <div className='api_name'>{ids.name}</div>
+        <div>
+          <span className='api_inline'>
+            <img src={imgC} alt='사진' />
+            {ids.cookingTime}분
+          </span>
+          <span className='api_inline'>
+            <img src={imgD} alt='사진' />
+            {ids.servings}인분
+          </span>
+          <span className='api_inline'>
+            <img src={imgE} alt='사진' />
+            {ids.kcal}kcal
+          </span>
+        </div>
+        <div className='line'>
+          <span className='api_block'>재료</span>
+          <ul>
+            {ids.ingredients &&
+              ids.ingredients.map((index) => (
+                <li>
+                  <img src={imgF} alt='사진' />
+                  {index}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className='line'>
+          <span className='api_block'>양념장</span>
+          <ul>
+            {ids.spices &&
+              ids.spices.map((index) => (
+                <li>
+                  <img src={imgF} alt='사진' />
+                  {index}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className='line'>
+          <span className='api_block'>만들어봅시다!!</span>
+          <ul>
+            {ids.cookingSteps &&
+              ids.cookingSteps.map((index, i) => (
+                <li>
+                  <div className='api_num'>{i}</div>
+                  {index}
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
