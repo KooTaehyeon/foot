0c6483ef8f5f5241f13d024c5b2df1ec5487a578
Kth <vcro12123@naver.com>
2022-01-27 11:23:21 +0900

    테스트

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import imgA from './img/eye@2x.png';
 import imgB from './img/time_gray@2x.png';
 import { Link } from 'react-router-dom';
 const Main = () => {
-  const [date, setDate] = useState([]);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [veiw, setView] = useState(0);
   //카테고리 담아두는곳
@@ -18,29 +18,29 @@ const Main = () => {
         'https://asia-northeast1-sharexpere-prod.cloudfunctions.net/recipe'
       )
     ).json();
-    setDate(json);
+    setData(json);
   };
   useEffect(() => {
     get();
     setLoading(false);
   }, []);
-  // console.log(date);
+  // console.log(data);
   //카테고리 별 이름 정렬하기
-  const categoryKor = date.filter((category) => category.category === 'korean');
-  const categoryJapan = date.filter(
+  const categoryKor = data.filter((category) => category.category === 'korean');
+  const categoryJapan = data.filter(
     (category) => category.category === 'japanese'
   );
   categoryKor.sort((a, b) => a.name.localeCompare(b.name));
   categoryJapan.sort((a, b) => a.name.localeCompare(b.name));
-  date.sort((a, b) => a.name.localeCompare(b.name));
-  date.sort((a, b) => a.category.localeCompare(b.category));
+  data.sort((a, b) => a.name.localeCompare(b.name));
+  data.sort((a, b) => a.category.localeCompare(b.category));
 
   console.log(categoryKor);
   console.log(categoryJapan);
-  console.log('필터', date);
+  console.log('필터', data);
 
-  const jsonDate = date.map((item) => (
-    <div className='box'>
+  const jsonDateJapan = categoryJapan.map((item) => (
+    <div className='box' key={categoryJapan.length}>
       <div className='img_box'>
         {' '}
         <img className='img_in_box' src={item.picture} alt='사진' />
@@ -63,15 +63,41 @@ const Main = () => {
       </div>
     </div>
   ));
-
+  const jsonDateKor = categoryKor.map((item) => (
+    <div className='box' key={categoryKor.length}>
+      <div className='img_box'>
+        {' '}
+        <img className='img_in_box' src={item.picture} alt='사진' />
+      </div>
+      <div className='api_info'>
+        <div className='api_title' id={item.id}>
+          <Link className='link_color' to={`/recipe/${item.id}`}>
+            {' '}
+            {item.name}
+          </Link>
+        </div>
+        <div className='api_time'>
+          <img className='time' src={imgB}></img>
+          {item.cookingTime}
+        </div>
+        <div className='api_view'>
+          <img className='cook_img' src={imgA}></img>
+          {veiw}
+        </div>
+      </div>
+    </div>
+  ));
+  console.log(data[0].category);
   return (
     <div className='all'>
       {loading === true ? (
         <h1>로딩중입니다</h1>
       ) : (
         <>
-          <div className='categroy_title'>{date[0].category}</div>
-          {jsonDate}
+          {/* <div className='categroy_title'>{data[1] && data[1].category}</div> */}
+          {jsonDateJapan}
+          {/* <div className='categroy_title'>{data[3].category}</div> */}
+          {jsonDateKor}
         </>
       )}
     </div>
